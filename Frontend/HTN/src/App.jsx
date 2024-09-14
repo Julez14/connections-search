81d21ead49584ff602fd815c461a592876c2213e
zhaju <juleoan123@gmail.com>
2024-09-14 12:09:31 -0400

    added graphs

--- Frontend/HTN/src/App.jsx
+++ Frontend/HTN/src/App.jsx
@@ -3,13 +3,22 @@ import Card from './Components/Card'
 import Navbar from './Components/Navbar'
 import './App.css'
 
+// new
+import React from 'react';
+import NetworkGraph from './NetworkGraph';
+
 function App() {
 
   return (
     <div>
       <Navbar/>
       <Card/>
-      
+
+      <h1>Network Graph with D3.js in React</h1>
+      <div className="graph-container">
+        <NetworkGraph />
+        <NetworkGraph />
+      </div> 
     </div>
   )
 }
