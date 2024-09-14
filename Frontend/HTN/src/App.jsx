acdba014aba462df465d9aeb4ba8106d44d45462
Julez14 <juelzlax@gmail.com>
2024-09-14 12:18:56 -0400

    convex setup

--- Frontend/HTN/src/App.jsx
+++ Frontend/HTN/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import Card from './Components/Card'
-import Navbar from './Components/Navbar'
-import './App.css'
+import { useState } from "react";
+import Card from "./Components/Card";
+import Navbar from "./Components/Navbar";
+import "./App.css";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 function App() {
-
   return (
     <div>
-      <Navbar/>
-      <Card/>
-      
+      <Navbar />
+      <Card />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
